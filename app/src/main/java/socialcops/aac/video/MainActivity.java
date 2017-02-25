cc27d8931e71963f6bf7f8f5915880d82723eb32
Ayush Chauhan <aac9095@gmail.com>
2017-02-25 14:43:53 +0530

    stream and download video

--- app/src/main/java/socialcops/aac/video/MainActivity.java
+++ app/src/main/java/socialcops/aac/video/MainActivity.java
@@ -1,18 +1,40 @@
 package socialcops.aac.video;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 import com.crashlytics.android.Crashlytics;
-import com.danikula.videocache.HttpProxyCacheServer;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import io.fabric.sdk.android.Fabric;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static socialcops.aac.video.R.id.video;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "Android Video Task";
+    private static final String TAG = "Video";
+    private String videoName = "header-img-background_video-1920-480.mp4";
     private final String VIDEO_URL = "https://socialcops.com/images/spec/home/header-img-background_video-1920-480.mp4";
     private VideoView videoView;
 
@@ -20,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        checkPermissions();
         try{
             final Fabric fabric = new Fabric.Builder(this)
                     .kits(new Crashlytics())
@@ -31,10 +53,135 @@ public class MainActivity extends AppCompatActivity {
             Log.e(TAG,"Exception",e);
         }
 
-        videoView = (VideoView) findViewById(R.id.video);
+        videoView = (VideoView) findViewById(video);
+        downLoadVideo();
+        try {
+            Uri uri = Uri.parse(VIDEO_URL);
+            // Start the MediaController
+            MediaController mediacontroller = new MediaController(this);
+            mediacontroller.setAnchorView(videoView);
+            videoView.setMediaController(mediacontroller);
+            videoView.setVideoURI(uri);
 
-        HttpProxyCacheServer proxy = Proxy.getProxy(this);
-        String proxyUrl = proxy.getProxyUrl(VIDEO_URL);
-        videoView.setVideoPath(proxyUrl);
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+            e.printStackTrace();
+        }
+
+        videoView.requestFocus();
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                videoView.start();
+            }
+        });
+    }
+
+    private void downLoadVideo() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://socialcops.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        VideoDownloadService videoDownloadService = retrofit.create(VideoDownloadService.class);
+
+        Call<ResponseBody> call = videoDownloadService.downloadVideo(VIDEO_URL);
+
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if (response.isSuccessful()) {
+                    Log.d(TAG, "server contacted and has file");
+
+                    boolean writtenToDisk = writeResponseBodyToDisk(response.body());
+
+                    Log.d(TAG, "file download was a success? " + writtenToDisk);
+                } else {
+                    Log.d(TAG, "server contact failed");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Log.e(TAG, "error",t);
+            }
+        });
+    }
+
+    private boolean writeResponseBodyToDisk(ResponseBody body) {
+        try {
+            File videoDir = getDir();
+            if(videoDir == null){
+                return false;
+            }
+            String video = videoDir.getPath() + File.separator + videoName;
+            File videoFile = new File(video);
+
+            InputStream inputStream = null;
+            OutputStream outputStream = null;
+
+            try {
+                byte[] fileReader = new byte[4096];
+
+                long fileSize = body.contentLength();
+                long fileSizeDownloaded = 0;
+
+                inputStream = body.byteStream();
+                outputStream = new FileOutputStream(videoFile);
+
+                while (true) {
+                    int read = inputStream.read(fileReader);
+
+                    if (read == -1) {
+                        break;
+                    }
+
+                    outputStream.write(fileReader, 0, read);
+
+                    fileSizeDownloaded += read;
+
+                    Log.d(TAG, "file download: " + fileSizeDownloaded + " of " + fileSize);
+                }
+
+                outputStream.flush();
+
+                return true;
+            } catch (IOException e) {
+                Log.e(TAG,"error",e);
+                return false;
+            } finally {
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+
+                if (outputStream != null) {
+                    outputStream.close();
+                }
+            }
+        } catch (IOException e) {
+            Log.e(TAG,"error",e);
+            return false;
+        }
+    }
+
+    public static File getDir() {
+        File sdDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        sdDir =  new File(sdDir, "SocialCops");
+        if (!sdDir.exists() && !sdDir.mkdirs()) {
+            Log.d(TAG, "Can't create directory to save video.");
+            return null;
+        } else {
+            return sdDir;
+        }
+    }
+
+    private void checkPermissions() {
+        int hasStoragePermission = ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if(hasStoragePermission != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    10);
+        }
     }
 }
